92a3e98ab07f3f83e08aef72f10b8ebbbeb57c0a
michael butler <michael.butler01@liberty-it.co.uk>
2022-07-19 12:21:40 +0100

    adding header/footer

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,13 +3,23 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {SignInComponent} from "../sign-in/sign-in.component";
 import {HomeComponent} from "../home/home.component";
-
+import {CreateAccountComponent} from "../create-account/create-account.component";
+import {DriverDetailsComponent} from "../driver-details/driver-details.component";
+import {DriverSummaryComponent} from "../driver-summary/driver-summary.component";
+import {MyBookingsComponent} from "../my-bookings/my-bookings.component";
+import {PassengerResultsComponent} from "../passenger-results/passenger-results.component";
+import {PassengerSummaryComponent} from "../passenger-summary/passenger-summary.component";
 export const routes: Routes = [
   {path: 'signIn', component: SignInComponent},
-  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'account', component: CreateAccountComponent},
+  {path: 'driver', component: DriverDetailsComponent},
+  {path: 'dSummary', component: DriverSummaryComponent},
+  {path: 'bookings', component: MyBookingsComponent},
+  {path: 'pResults', component: PassengerResultsComponent},
+  {path: 'pSummary', component: PassengerSummaryComponent},
   {path: '**', redirectTo: ''}
 ];
-
 @NgModule({
   declarations: [],
   imports: [
