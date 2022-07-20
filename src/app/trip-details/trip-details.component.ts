bdb26f937d8af489cc3cbd5297482f874fcda316
Mark Millsopp <mark.millsopp@liberty-it.co.uk>
2022-07-20 10:11:36 +0100

    Validation working on trip details

--- src/app/trip-details/trip-details.component.ts
+++ src/app/trip-details/trip-details.component.ts
@@ -8,27 +8,16 @@ import {Trip} from "../model/trip";
   styleUrls: ['./trip-details.component.css']
 })
 export class TripDetailsComponent implements OnInit {
-  //trip!: Trip;
-
-  // Line below needs looked at again
- // @Output() tripEntered = new EventEmitter<Trip>();
   title = 'ReactiveForms';
   tripForm: FormGroup;
   submitted = false;
 
-  // ngOnInit() {
-  //   this.tripForm = new FormGroup({
-  //     tripPostcode: new FormControl(null),
-  //     tripDestination: new FormControl(null),
-  //     tripDate: new FormControl(null),
-  //     tripTime: new FormControl(null),
-  //   })
-  // }
-  //private trip: Trip;
   onSubmit(): void {
     this.submitted = true;
+    if(!this.tripForm.valid) {
+      return;
+    }
     console.log(this.tripForm);
-    //this.tripEntered.emit(this.trip);
   }
   constructor(private formBuilder: FormBuilder) {
     this.tripForm = formBuilder.group({
@@ -40,11 +29,6 @@ export class TripDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-   // this.updateFromModel(this.trip);
-  }
-  get registerTripControl() {
-    return this.tripForm.controls;
-  }
-  get getTripPostcode() { return this.tripForm.get('tripPostcode'); }
 
+  }
 }
