2f1755b6cf91dc09d31521de1f44143911e746f6
“kingh247” <kingh-j@ulster.ac.uk>
2022-08-02 11:02:35 +0100

    added summary page to local host http client

--- src/app/passenger-summary/passenger-summary.component.ts
+++ src/app/passenger-summary/passenger-summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-passenger-summary',
@@ -9,31 +10,32 @@ export class PassengerSummaryComponent implements OnInit {
   pageTitle: string = "Summary Screen";
 
 
-
-  drivers: any[] = [
-    {"name": "Alfred Hitchcock"},
-    {"name": "Bruce Wayne"},
-    {"name": "Darth Vader"},
-    {"name": "Peter Pan"}
-  ]
-
-  summary1: any[] = [
-    {"name": "Driver name: Bruce Wayne"},
-    {"name": "Driver Registration: WUI 9999"},
-    {"name": "Pick up point:   Nerwy"},
-    {"name": "pick location: Nerwy"},
-    {"name": "Pick up time:  10:00am"},
-    {"name": "Return: no"}
-  ]
+  trips:any;
+  // drivers: any[] = [
+  //   {"name": "Alfred Hitchcock"},
+  //   {"name": "Bruce Wayne"},
+  //   {"name": "Darth Vader"},
+  //   {"name": "Peter Pan"}
+  // ]
+  //
+  // summary1: any[] = [
+  //   {"name": "Driver name: Bruce Wayne"},
+  //   {"name": "Driver Registration: WUI 9999"},
+  //   {"name": "Pick up point:   Nerwy"},
+  //   {"name": "pick location: Nerwy"},
+  //   {"name": "Pick up time:  10:00am"},
+  //   {"name": "Return: no"}
+  // ]
   onClick() {
-    console.log(this.drivers)
+    // console.log(this.drivers)
   }
-  constructor() { }
-
+  constructor(private http:HttpClient) { }
 
 
   ngOnInit(): void {
 
+    let response = this.http.get("http://localhost:8080/people/")
+    response.subscribe((data)=>this.trips=data);
 
 
 
