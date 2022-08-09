3c3f16fde235c20f38647db1b3ab2f6e4de9c664
michael butler <michael.butler01@liberty-it.co.uk>
2022-08-09 09:42:22 +0100

    :wc :wq

--- src/app/confirmation-page/confirmation-page.component.ts
+++ src/app/confirmation-page/confirmation-page.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {TripService} from "../services/trip.service";
 import {HttpClient} from "@angular/common/http";
 import {CarService} from "../services/car.service";
+import {Car} from "../model/car";
+import {Person} from "../model/person";
+import {PeopleService} from "../services/person.service";
+import {Trip} from "../model/trip";
 
 @Component({
   selector: 'app-confirmation-page',
@@ -11,6 +15,9 @@ import {CarService} from "../services/car.service";
 export class ConfirmationPageComponent implements OnInit {
 
   results: any =[];
+  currentCar!: Car;
+  currentDriver!: Person;
+  driverTripSelected!: Trip;
   // summary2: any[] = [
   //   {"name": "Driver name: Bruce Wayne"},
   //   {"name": "Car Make: Audi"},
@@ -23,16 +30,40 @@ export class ConfirmationPageComponent implements OnInit {
   // ]
 
 
-  constructor(public tripService: TripService,private http:HttpClient, public carService: CarService) {
+  constructor(public tripService: TripService,private http:HttpClient, public carService: CarService, private peopleService: PeopleService) {
 
 
   }
   ngOnInit(): void {
     // let response = this.http.get("http://localhost:8080/cars")
     // response.subscribe((data)=>this.results=data);
+    this.fetchDetails()
     this.tripService.getAllTrips()
       .subscribe(trips => trips.forEach( trip => this.results.push(trip)))
 
+  }
+
+  private delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  private async fetchDetails()
+  {
+    this.carService.getCarById(this.tripService.driverTripSelected.carId)
+      .subscribe(car => this.currentCar = car)
+    await this.delay(100);
+    this.carService.currentCar = this.currentCar
+
+    this.peopleService.getPersonById(this.currentCar.ownerId)
+      .subscribe(person => this.currentDriver = person)
+
+    this.driverTripSelected = this.tripService.driverTripSelected;
+  }
+
+  onSubmit(){
+    this.tripService.currentTrip.carId = this.tripService.driverTripSelected.carId
+    console.log(this.tripService.currentTrip.carId)
+    this.tripService.addTrip(this.tripService.currentTrip).subscribe((data) => {});
   }
 
 
